33027e7b20bb9010fedabcbe748247b9c12509fc
pashokman <pashokman1@gmail.com>
2022-07-18 19:44:46 +0300

    my first test project
    it's a few functions

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a few functions that chaked if input number is a perfect number 
+- https://en.wikipedia.org/wiki/Perfect_number
+- https://en.wikipedia.org/wiki/List_of_Mersenne_primes_and_perfect_numbers
---
+++ main.js
@@ -0,0 +1,30 @@
+const perfect = (number) => {
+    let result = 0;
+    
+    for (let index = 1; index < number; index++) {
+      if (number % index === 0) result += index;
+    }
+  
+    if (result === number) {
+      return true;
+    }
+    return false;
+  }
+  
+  const isPerfect = (number) => (perfect(number)) ? console.log('true') : console.log('false');
+
+isPerfect(1);
+isPerfect(2);
+isPerfect(3);
+isPerfect(4);
+isPerfect(5);
+isPerfect(6);   //true
+isPerfect(7);
+isPerfect(8);
+isPerfect(9);
+isPerfect(10);
+isPerfect(28);  //true
+isPerfect(30);
+isPerfect(35);
+isPerfect(37);
+isPerfect(496); //true
